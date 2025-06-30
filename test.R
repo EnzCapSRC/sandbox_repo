6d580d4f87cb5dd94cc60e39f480e0c7c147c0df
EnzCapSRC <ecapistrano@srcentre.com.au>
2025-06-30 15:49:15 +1000

    Adding test.R file

---
+++ test.R
@@ -0,0 +1,25 @@
+#' ---
+#' title: test.R
+#' author: EnzCapSRC
+#' created: 2025-06-30
+#' output:
+#'     html_document:
+#'         toc: true
+#'
+#' overview: |
+#'     This is a test file for learning GitHub features.
+#' ---
+
+srcproj::load_settings()
+
+# Section 1 ----
+
+do_stuff
+
+# Section 2 ----
+
+im_stuff
+
+#---
+#EOF
+#---
